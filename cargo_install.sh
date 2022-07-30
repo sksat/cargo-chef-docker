215e8506fe3b2d4e32c97bbe7c5a346640cfac12
sksat <sksat@sksat.net>
2022-07-30 21:33:22 +0900

    add: manual cargo install alternative script

---
+++ cargo_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function download_crate() {
+	CRATE="$1"
+	VERSION="$2"
+
+	mkdir download || exit 1
+	cd download || exit 1
+	curl -L https://crates.io/api/v1/crates/${CRATE}/${VERSION}/download | gzip -d | tar -xf -
+	mv "${CRATE}-${VERSION}" ..
+	cd .. || exit 1
+	rmdir download
+}
+
+# install crate without cargo install
+function install_without_install() {
+	CRATE="$1"
+	VERSION="$2"
+
+	download_crate "$CRATE" "$VERSION"
+	cd "${CRATE}-${VERSION}" || exit 1
+	cargo build --release --locked
+	cp "target/release/${CRATE}" "$CARGO_HOME/bin"
+	cd .. || exit 1
+	rm -rf "${CRATE}-${VERSION}"
+}
+
+install_without_install "$1" "$2"
